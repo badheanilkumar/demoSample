7734d1b6889d0ac14a0aa301b3363aebddb2eb9f
badhe <badheanilkumar@gmail.com>
2022-12-05 15:22:47 +0530

    project added

---
+++ src/main/java/com/example/demoSample/Controller.java
@@ -0,0 +1,13 @@
+package com.example.demoSample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/Helo")
+    public String hello(){
+        return "hello";
+    }
+}
